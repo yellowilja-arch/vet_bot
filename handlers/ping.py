6d9d675da863d4d22f8ca075371173a4b9f4db6c
yellowilja-arch <yellow.ilja@gmail.com>
2026-04-17 23:37:02 +0300

    Add files via upload

--- handlers/ping.py
+++ handlers/ping.py
@@ -7,3 +7,7 @@ router = Router()
 @router.message(Command("ping"))
 async def ping_command(message: Message):
     await message.answer("pong")
+
+@router.message(Command("stats"))
+async def stats_command(message: Message):
+    await message.answer("✅ stats работает!")
