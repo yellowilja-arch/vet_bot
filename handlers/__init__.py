55633a347ef5693c365a363a5e2073db3bb20124
yellowilja-arch <yellow.ilja@gmail.com>
2026-04-18 02:28:17 +0300

    Add files via upload

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,11 +2,9 @@ from .common import router as common_router
 from .client import router as client_router
 from .doctor import router as doctor_router
 from .admin import router as admin_router
-from .ping import router as ping_router  # опционально
 
 def register_handlers(dp):
     dp.include_router(common_router)
     dp.include_router(client_router)
     dp.include_router(doctor_router)
     dp.include_router(admin_router)
-    dp.include_router(ping_router)  # если хочешь оставить /ping
