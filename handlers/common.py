2653c736956b877984c95641c349517519a85fe0
yellowilja-arch <yellow.ilja@gmail.com>
2026-04-18 03:01:15 +0300

    Add files via upload

--- handlers/common.py
+++ handlers/common.py
@@ -2,8 +2,16 @@ import redis
 from aiogram import Router, F
 from aiogram.filters import Command
 from aiogram.types import Message
-from config import REDIS_URL
-
+from aiogram.fsm.context import FSMContext
+from config import TOPICS, REDIS_URL
+from services.validators import is_blocked, is_doctor, get_doctor_status, get_current_client, set_doctor_status, update_doctor_activity
+from database.doctors import get_all_doctors
+from database.queue import get_queue_length
+from database.users import save_user_if_new
+from utils.helpers import safe_send_message, safe_send_photo, get_anonymous_id
+from keyboards.client import get_client_main_keyboard
+from keyboards.doctor import get_doctor_main_keyboard, get_doctor_status_keyboard
+from states.forms import PaymentState
 r = redis.from_url(REDIS_URL, decode_responses=True)
 router = Router()
 
